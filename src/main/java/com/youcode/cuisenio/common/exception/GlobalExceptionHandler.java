a44702c25703d109b36891c591698f99e7eb80b8
Douaa <douaachemnane@gmail.com>
2025-03-11 12:04:53 +0000

    add exeption handler to advice controller

--- src/main/java/com/youcode/cuisenio/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/youcode/cuisenio/common/exception/GlobalExceptionHandler.java
@@ -2,16 +2,23 @@ package com.youcode.cuisenio.common.exception;
 
 import com.youcode.cuisenio.common.exception.base.ValidationException;
 import com.youcode.cuisenio.features.auth.exception.UserAlreadyExistsException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 @RestControllerAdvice
@@ -55,5 +62,70 @@ public class GlobalExceptionHandler {
         return ApiError.of("INTERNAL_SERVER_ERROR", "Une erreur interne est survenue");
     }
 
+
+
+    /**
+     * Handles exceptions for invalid method arguments annotated with @Valid.
+     * For example, when input data does not meet validation constraints.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        Map<String, Object> responseBody =  Map.of("errors", errors);
+
+        return ResponseEntity.badRequest().body(responseBody);
+    }
+
+    /**
+     * Handles exceptions when validation constraints are violated in @Validated methods.
+     * For example, when a method parameter violates @Size, @NotNull, etc.
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = ex.getConstraintViolations().stream()
+                .collect(Collectors.toMap(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage));
+        Map<String, Object> responseBody =  Map.of("errors", errors);
+        return ResponseEntity.badRequest().body(responseBody);
+    }
+
+    /**
+     * Handles exceptions when the HTTP method is not supported by the endpoint.
+     * For example, sending a POST request to an endpoint that only supports GET.
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        String message = "HTTP method not supported: " + ex.getMethod();
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(message);
+    }
+
+
+    /**
+     * Handles exceptions when an illegal argument is passed to a method.
+     * For example, passing a null or invalid value where a valid value is required.
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+
+    /**
+     * Handles exceptions when no handler is found for the requested URL and HTTP method.
+     * For example, accessing a non-existent endpoint.
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        String message = "No handler found for " + ex.getHttpMethod() + " " + ex.getRequestURL();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentialsException(BadCredentialsException ex) {
+        Map<String, String> errors = Map.of("message", "Incorrect password. please try again or click to reset your password");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errors);
+    }
 }
 
